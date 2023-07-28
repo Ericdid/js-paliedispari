f861fc63ce08047d3f7918a1694bc17ab09425bb
Ericdid <ericdidom@gmail.com>
2023-07-28 11:30:52 +0200

    add.pariodispari

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,19 +1,26 @@
 //l'utente sceglie pari o dispari
-const userEvenOrOdd = parseInt(
+let userEvenOrOdd = parseInt(
   prompt("seleziona un numero,se è pari sarai pari altrimenti sarai dispari")
 );
-
+//L'UTENTE SCEGLIE PARI O DISPARI
+while (isNaN(userEvenOrOdd)) {
+  userEvenOrOdd = parseInt(
+    prompt("seleziona un numero,se è pari sarai pari altrimenti sarai dispari")
+  );
+}
 if (userEvenOrOdd % 2 == 0) {
+  userEvenOrOdd = true;
   console.log("sei pari!");
 } else {
+  userEvenOrOdd = false;
   console.log("sei dispari!");
 }
 
 // l utente sceglie un numero da 1 a 5
-userNumber = parseInt(prompt("seleziona un numero da 1 a 5"));
+let userNumber = parseInt(prompt("seleziona un numero da 1 a 5"));
 
 // L UTENTE SCEGLIE UN NUMERO DA 1 A 5
-while (userNumber > 5 || userNumber == 0) {
+while (userNumber > 5 || userNumber == 0 || isNaN(userNumber)) {
   userNumber = parseInt(prompt("seleziona un numero da 1 a 5"));
 }
 
@@ -39,16 +46,21 @@ console.log("la somma è: " + sum);
 
 // stabiliamo se la somma dei due numeri è pari o dispari (funzione)
 function oddOrEven() {
-  if (sum % 2 === 0) {
+  if (sum % 2 == 0) {
     console.log("la somma è pari");
+    return true;
   } else {
     console.log("la somma è dispari");
+    return false;
   }
 }
 //richiamo la funzione
 oddOrEven(1);
 
-if (even == oddOrEven) {
+//mantengo il pari o dispari
+const result = oddOrEven(1);
+
+if (userEvenOrOdd == result) {
   console.log("hai vinto!");
 } else {
   console.log("hai perso");
